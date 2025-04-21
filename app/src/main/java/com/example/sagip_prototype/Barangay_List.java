e44b24dc0bd8d3afc6f0ee13f52ce0eb9d221b7c
sagipthesis <compscidev01@gmail.com>
2025-04-22 07:33:49 +0800

    updated last

--- app/src/main/java/com/example/sagip_prototype/Barangay_List.java
+++ app/src/main/java/com/example/sagip_prototype/Barangay_List.java
@@ -1,5 +1,6 @@
 package com.example.sagip_prototype;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Barangay_List extends AppCompatActivity {
 
     @Override
@@ -15,6 +18,27 @@ public class Barangay_List extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_barangay_list);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
+        bottomNavigationView.setSelectedItemId(R.id.barangay_seniorList);
 
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.barangay_seniorList) {
+                return true;
+            } else if (itemId == R.id.barangay_dashboard) {
+                startActivity(new Intent(getApplicationContext(), Barangay_Dashboard.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (itemId == R.id.barangay_profile) {
+                startActivity(new Intent(getApplicationContext(), Barangay_Profile.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            }
+            return false;
+        });
     }
+
 }
--- app/src/main/java/com/example/sagip_prototype/Barangay_Profile.java
+++ app/src/main/java/com/example/sagip_prototype/Barangay_Profile.java
@@ -1,6 +1,9 @@
 package com.example.sagip_prototype;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,17 +12,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Barangay_Profile extends AppCompatActivity {
+        FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_barangay_profile);
         TextView labelProfile = findViewById(R.id.profileName);
         TextView email = findViewById(R.id.profileEmail);
 
+        LinearLayout gotoUpdate = findViewById(R.id.gotoupdate);
+        LinearLayout gotoLogut = findViewById(R.id.logoutLayout);
 
+        gotoUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Barangay_Profile.this, Barangay_Registration.class);
+                startActivity(intent);
+            }
+        });
 
+        gotoLogut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(Barangay_Profile.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
+        bottomNavigationView.setSelectedItemId(R.id.barangay_profile);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.barangay_profile) {
+                return true;
+            } else if (itemId == R.id.barangay_seniorList) {
+                startActivity(new Intent(getApplicationContext(), Barangay_List.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (itemId == R.id.barangay_dashboard) {
+                startActivity(new Intent(getApplicationContext(), Barangay_Dashboard.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            }
+            return false;
+        });
     }
+
 }
